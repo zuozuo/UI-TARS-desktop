b2203cff466fdb138f91ed15020b5d3c15e29586
jinxin001 <jinxin001@bytedance.com>
2025-02-12 19:49:01 +0800

    chore: menu label

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -54,17 +54,17 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'UI-TARS Desktop',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About UI-TARS Desktop',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide UI-TARS Desktop',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
